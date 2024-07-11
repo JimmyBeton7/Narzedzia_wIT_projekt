25309a562981e22b760048cb1f3cd9e42f06f3fc
JimmyBeton7 <mateusz.durka77@gmail.com>
2024-07-11 19:58:14 +0200

    Task2: Added JSON file loading and validation

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def parse_arguments():
     if len(sys.argv) != 3:
@@ -10,7 +11,20 @@ def parse_arguments():
 
     return input_file, output_file
 
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            print("JSON file loaded successfully")
+            return data
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+
 if __name__ == "__main__":
     input_file, output_file = parse_arguments()
-    print(f"Input file: {input_file}")
-    print(f"Output file: {output_file}")
+    data = load_json(input_file)
+    print(data)
